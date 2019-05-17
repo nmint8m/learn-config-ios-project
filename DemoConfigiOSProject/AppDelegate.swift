b34c4a47e7e2d36035bb558a8b5fc0a8abc9277d
Tam Nguyen M <nmint8m@gmail.com>
2019-05-17 16:47:40 +0700

    Clean empty lines

--- DemoConfigiOSProject/AppDelegate.swift
+++ DemoConfigiOSProject/AppDelegate.swift
@@ -15,12 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .white
         window?.rootViewController = ViewController()
-
         return true
     }
 }
